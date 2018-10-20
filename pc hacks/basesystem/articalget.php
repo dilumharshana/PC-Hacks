9cbcc4a211daa2031b46ac9ecaeed55944049055
dilumharshana <dilum.harshana123@gmail.com>
2018-10-20 22:27:05 +0530

    final

---
+++ pc hacks/basesystem/articalget.php
@@ -0,0 +1,10 @@
+<?php
+	$fname = $_post['fname'];
+	$lname = $_post['lname'];
+	$title = $_post['subject'];
+	$email = $_post['email'];
+	$phoneno = $_post['phoneno'];
+	$tute = $_post['tute'];
+
+	echo $fname .<br.>. $email;
+?>
